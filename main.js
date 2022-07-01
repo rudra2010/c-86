2c50dbe44db8d4fb3aebc5adb7e92778c68cd5a7
rudra2010 <102788949+rudra2010@users.noreply.github.com>
2022-07-01 21:03:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ fabric.Image.fromURL("BirthdayImage.jpg" , function(Img) {
     block_image_object= Img;
 
 
-    block_image_object.scaleToWidth(699);
-    block_image_object.scaleToheight(510);
+    block_image_object.scaleToWidth(1000);
+    block_image_object.scaleToHeight(800);
     block_image_object.set({
         top:0,
         left:0
